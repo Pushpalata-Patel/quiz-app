2b6da14ad24c3a9ea2b283b4f865f6c84756e9fd
Pushpalata-Patel <pushp1027@outlook.com>
2024-08-04 12:24:15 +0530

    changes

---
+++ src/assets/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        question: "What is rank of India in Travel & Tourism Development Index 2024?",
+        option1: "36th",
+        option2: "37th",
+        option3: "38th",
+        option4: "39th",
+        ans: 4,
+    },
+    {
+        question: "Recently, where was the 52nd Conference of Governors held?",
+        option1: "Hyderabad",
+        option2: "New Delhi",
+        option3: "Chennai",
+        option4: "Bengaluru",
+        ans: 2,
+    },
+    {
+        question: "Which country hosts the international air exercise Tarang Shakti 2024?",
+        option1: "UK",
+        option2: "India",
+        option3: "Germany",
+        option4: "France",
+        ans: 2,
+    },
+    {
+        question: "Krishnaraja Sagar (KRS) Dam, recently seen in the news, is located in which state?",
+        option1: "Karnataka",
+        option2: "Tamil Nadu",
+        option3: "Gujarat",
+        option4: "Kerala",
+        ans: 1,
+    },
+    {
+        question: "Jhumur, recently seen in the news, is a traditional dance performed in which state?",
+        option1: "Nagaland",
+        option2: "Sikkim",
+        option3: "Assam",
+        option4: "Manipur",
+        ans: 3,
+    }
+
+]
+
